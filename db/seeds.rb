2c317a665a77dc32286c337f516c705472975d3b
Justin Valentini <justin.valentini@gmail.com>
2016-05-02 18:39:24 -0400

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+TechTalk.create(title: "Founder School Session: The Future Doesn't Have to Be Incremental", length: 5, views: 0, link: 'https://www.youtube.com/watch?v=gTAghAJcO1o')

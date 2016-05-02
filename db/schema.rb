cde4867388388a8b652d7f76c7cf8dcf4f4e6106
Justin Valentini <justin.valentini@gmail.com>
2016-05-02 16:23:42 -0400

    Drop movies table.

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160502184211) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "director"
-    t.integer  "length"
-    t.float    "avg_rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "tech_talks", force: :cascade do |t|
     t.string   "title"
     t.text     "description"

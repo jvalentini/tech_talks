0ace6a913baa881a5896c8e968fb7e543426cd91
Justin Valentini <justin.valentini@gmail.com>
2016-05-04 15:27:04 -0400

    Fix failing view spec.

--- spec/views/tech_talks/edit.html.erb_spec.rb
+++ spec/views/tech_talks/edit.html.erb_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'tech_talks/edit', type: :view do
-  let(:tech_talk) { assign(:tech_talk, TechTalk.create!(title: 'My first talk')) }
+  let(:tech_talk) { TechTalk.create!(title: 'My first talk') }
 
   it 'renders the edit tech_talk form' do
+    assign(:tech_talk, tech_talk)
     render
 
     assert_select 'form[action=?][method=?]', tech_talk_path(tech_talk), 'post' do

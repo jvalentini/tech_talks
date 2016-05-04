2f7baf1675fa4a08ecff474a87e073c65a4e2559
Justin Valentini <justin.valentini@gmail.com>
2016-05-04 14:58:50 -0400

    Add rspec rubocop and fix warnings.

--- spec/views/tech_talks/edit.html.erb_spec.rb
+++ spec/views/tech_talks/edit.html.erb_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'tech_talks/edit', type: :view do
-  before(:each) do
-    @tech_talk = assign(:tech_talk, TechTalk.create!(title: 'My first talk'))
-  end
+  let(:tech_talk) { assign(:tech_talk, TechTalk.create!(title: 'My first talk')) }
 
   it 'renders the edit tech_talk form' do
     render
 
-    assert_select 'form[action=?][method=?]', tech_talk_path(@tech_talk), 'post' do
+    assert_select 'form[action=?][method=?]', tech_talk_path(tech_talk), 'post' do
     end
   end
 end
